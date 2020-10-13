6f6c09548530da84e083622596d4903ae68a5626
AdamWise <wise.adam.jay@gmail.com>
2020-10-13 13:14:25 -0400

    lots of new features

--- js/spectroSim.js
+++ js/spectroSim.js
@@ -21,6 +21,7 @@
  * a detector factory, where there's one pulldown for camera and one for spectrometer
  * 
  * I want to re-scale the x axis into wavelength...
+ * lets start by creating adjustable x bounds
  * 
  */
 
@@ -39,16 +40,21 @@
  var app = {
      'debug' : 1, // 
      'nSamples' : 1, // how many traces to draw per detector
-     'graphWidthmm' : 1, // width of plot area in mm
+     'graphMinXmm' : 0, // minimum x value of plot field of view (focal plane) in mm,
+     'graphMaxXmm' : 5, // minimum x value of plot field of view (focal plane) in mm,
      'svgWidth' : 700,
-     'svgHeight' : 400,
+     'svgHeight' : 280,
      'graphYMin' : -20,
      'graphYMax' : 5000,
-     'scaleTraces' : 1,
+     'scaleTraces' : 0, // correct intensity of traces by factor of 1/pixelsize to account for splitting counts
      'centerWavelength' : 500,
+     'svg' : d3.select('svg'),
+     'graphMarginX' : 30,
+     'graphMarginY' : 20,
+     'targetDispersion' : 10, // disperions for x axis in nm/mm.  sets scale of x axis
  }
 
- // ========================================= General Purpose Function ===================================
+ // ========================================= General Purpose Functions ===================================
 
  // gaussian function
  function g(x, mu = 0, sig = 1){
@@ -106,12 +112,12 @@ function poissonSample( lambda = 1){
          this.height = height;
      }
 
-     createSpectrumDataObject(camConfigObj, spectrometerConfigObj) {
+     createSpectrumDataObject(camConfigObj, spectrometerConfigObj, gratingConfigObj) {
          if (app.debug){console.log('createSpectrumDataObject called')};
          var dataArray = [];
          dataArray.length = camConfigObj['xPixels'];
          var pixelSize = camConfigObj['xPixelSize'];
-         var relDispersion = (spectrometerConfigObj['fl'] / 163) * (spectrometerConfigObj['gratingRule'] / 150)
+         var relDispersion = (spectrometerConfigObj['fl'] / 163) * (gratingConfigObj['rule'] / 150)
          dataArray.fill(0);
          // add the contribution from each peak
          for (var k in this.peakList){
@@ -135,31 +141,35 @@ function poissonSample( lambda = 1){
  }
 
  class Detector {
-     constructor(camConfigObj, spectrometerConfigObj, spectrumGenerator){
+     constructor(camConfigObj, spectrometerConfigObj, gratingConfigObj, spectrumGenerator){
         this.camConfigObj = camConfigObj;
         this.spectrometerConfigObj = spectrometerConfigObj;
-        this.spectrum = spectrumGenerator.createSpectrumDataObject(camConfigObj, spectrometerConfigObj);
+        this.gratingConfigObj = gratingConfigObj;
+        this.spectrum = spectrumGenerator.createSpectrumDataObject(camConfigObj, spectrometerConfigObj, gratingConfigObj);
         this.paths = [];
-        this.svg = d3.select('svg')
+        this.svg = app.svg;
         this.g = this.svg.append('g')
         this.line = d3.line().x(function(d,i){return i/10}).y(function(d){return d})
-
-        
-
      }
 
      draw(){
+
+
+        if (this.active==0){
+            return
+        }
+
          if (app.debug == 1){
             console.log('drawing')
          }
-         var xShift = 0;//-1 * this.configObj.xPixelSize/1000
-         var relDispersion = (this.spectrometerConfigObj['fl'] / 163) * (this.spectrometerConfigObj['gratingRule'] / 150)
-         var scaleX = d3.scaleLinear().domain([ (xShift), (app['graphWidthmm'] + xShift) * relDispersion ]).range([0,app.svgWidth])
+         var xShift = 0;//-1 * this.camera.xPixelSize/1000
+         var relDispersion = (this.spectrometerConfigObj['fl'] / 163) * (this.gratingConfigObj['rule'] / 150)
+         var scaleX = d3.scaleLinear().domain([ (app.graphMinXmm + xShift), (app.graphMaxXmm + xShift) * relDispersion ]).range([0 + app.graphMarginX , app.svgWidth - app.graphMarginX])
          
          var yScaleFactor = 1;
          if (app['scaleTraces']) {yScaleFactor = 25/this.camConfigObj.xPixelSize}; 
          
-         var scaleY = d3.scaleLinear().domain( [app['graphYMin'] / yScaleFactor, app['graphYMax'] / yScaleFactor ]).range([app['svgHeight'], 0]);
+         var scaleY = d3.scaleLinear().domain( [app['graphYMin'] / yScaleFactor, app['graphYMax'] / yScaleFactor ]).range([app['svgHeight'] - app.graphMarginY, 0 + app.graphMarginY]);
          this.line.x( (d,i)=>scaleX(i * this.camConfigObj['xPixelSize'] / 1000) );
          this.line.y(d=>scaleY(d))
         
@@ -179,6 +189,7 @@ function poissonSample( lambda = 1){
             this.paths.push(newPath);
             newPath.style('stroke-opacity', 2 * 1/app['nSamples'])
             newPath.attr('d', this.line(measuredData))
+            newPath.attr('clip-path','url(#clipPath)')
             newPath.style('stroke', this.graphColor)
          }
      }
@@ -188,18 +199,37 @@ function poissonSample( lambda = 1){
      }
  }
 
+class DetectorGroup {
+    constructor(){
+        this.detectors = [];
+    }
+
+    add(d){
+        this.detectors.push(d);
+    }
+
+    update(){
+        this.detectors.forEach(function(d){
+            d.erase();
+            d.draw();
+        })
+    }
+}
+
  // ====================================================================================
 
  var spectra = 0
  var data1 = 0;
 
- peakList1 = [{'a' :800, 'mu':0.06 , 'sigma':0.0001},
+ peakList1 = [{'a' :800, 'mu':26/1000 * 5 , 'sigma':0.0001},
                 {'a' :1920, 'mu':0.2, 'sigma':0.030/2.355},
                 {'a' :1920, 'mu':0.24, 'sigma':0.030/2.355},
                 {'a' :3820, 'mu':0.37, 'sigma':0.070/2.355},
 
                 {'a' :1820, 'mu':0.5, 'sigma':0.0001},
                 {'a' :1820, 'mu':0.75, 'sigma':0.0001},
+                {'a' :1820, 'mu':2, 'sigma':0.0001},
+                {'a' :1820, 'mu':3, 'sigma':0.0001},
             ]
 
  spectrumGen1 = new SpectrumGenerator(peakList = peakList1)
@@ -209,6 +239,17 @@ function poissonSample( lambda = 1){
 
 
 var mainSvg = d3.select('body').append('svg').style('height', app.svgHeight).style('width',app.svgWidth)
+app.svg = mainSvg;
+
+// create a clippath to establish drawing area of svg
+var clipPath = app.svg.append('clippath')
+clipPath.attr('id','clipPath')
+clipPath.append('rect')
+    .attr('x', app.graphMarginX + 'px')
+    .attr('y', app.graphMarginY + 'px')
+    .attr('width', app.svgWidth - 2*app.graphMarginX + 'px')
+    .attr('height', app.svgHeight - 2*app.graphMarginY  + 'px')
+
 
 /// create a new detector configuration and detector
 
@@ -216,9 +257,9 @@ var idealSpectrometer = spectrometers['Ideal Imaging System'];
 
 
 var idealCamConfig = {'readNoise' : 0, 'xPixels' : 2000, 'xPixelSize' : 1, 'ideal' : true,}
-var detector1 = new Detector(idealCamConfig, idealSpectrometer, spectrumGen1)
 
-//detector1.draw();
+//
+var allDetectors = new DetectorGroup();
 
 
 // detector factory goes here
@@ -259,22 +300,75 @@ createDetectorButton.on('click', function(){
     console.log('pressed')
     var newCamObj = cameraDefs[cameraSelect.property('value')];
     var newSpecObj = spectrometers[spectrometerSelect.property('value')];
-    newSpecObj['gratingRule'] = gratings[gratingSelect.property('value')]['rule']
+    var newGratingObj = gratings[gratingSelect.property('value')];
     
     console.log(newSpecObj)
     
-    var newDetector = new Detector(newCamObj, newSpecObj, spectrumGen1)
+    var newDetector = new Detector(newCamObj, newSpecObj, newGratingObj, spectrumGen1)
     newDetector.graphColor = `hsl(${Math.round(Math.random()*360)},100%,50%)`
-    newDetector.draw();
-    if(app.debug){console.log(detector1.graphColor)};
+    newDetector.active = 1;// this is a hack for display, fix eventually
+    
+    allDetectors.add(newDetector);
+    allDetectors.update(); 
+    //newDetector.draw();
+    //cameras.push(newDetector)
+    //cameras.forEach(f=>f.erase())
+    //cameras.forEach(f=>f.draw())
+
 
     d3.select('body')
         .append('div')
-        .text(newCamObj['displayName'] + ' - ' + newSpecObj['displayName'] + ' - ' + newSpecObj['gratingRule'] + ' lines / mm')
+        .text(newCamObj['displayName'] + ' - ' + newSpecObj['displayName'] + ' - ' + newGratingObj['rule'] + ' lines / mm')
         .style('color', newDetector.graphColor)
+        .style('font-size', '22px')
         .on('click', function(){
             newDetector.erase();
+            newDetector.active = 0;
             this.remove()
         })
 })
+
+
+// add gui elements for min and max wavelength display
+var minXinput = d3.select('#graphLimsGui')
+                    .append('input')
+                    .attr('value', app.graphMinXmm)
+                    .style('width', '40px')
+                    .on('input', function(){
+                        if (app.debug){console.log(this.value)}
+                        if( !isNaN(Number(this.value))){
+                            app.graphMinXmm = Number(this.value);
+                            allDetectors.update();
+                        }
+
+                    })
+
+var maxXinput = d3.select('#graphLimsGui')
+                    .append('input')
+                    .attr('value', app.graphMaxXmm)
+                    .style('width', '40px')
+                    .on('input', function(){
+                        if (app.debug){console.log(this.value)}
+                        if( !isNaN(Number(this.value))){
+                            app.graphMaxXmm = Number(this.value);
+                            allDetectors.update();
+                        }
+
+                    })
+
+// add axes
+
+var xScale = d3.scaleLinear().domain([ app.targetDispersion * app.graphMinXmm + app.centerWavelength , app.targetDispersion * app.graphMaxXmm + app.centerWavelength]).range([0 + app.graphMarginX, app.svgWidth - app.graphMarginX]);
+var yScale = d3.scaleLinear().domain([0,100]).range([app.svgHeight - app.graphMarginY, 0 + app.graphMarginY]);
+
+var xAxisG = app.svg.append('g')
+    .attr('transform', `translate(0,${app.svgHeight - app.graphMarginY})`)
+    
+var yAxisG = app.svg.append('g')
+    .attr('transform', `translate(${app.graphMarginX},0)`);
+var xAxis = d3.axisBottom(xScale);
+var yAxis = d3.axisLeft(yScale);
+
+xAxis(xAxisG);
+yAxis(yAxisG);
 
